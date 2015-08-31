78afa15e7e51f35683568ebb47829d22088382d3
Ming Ruan <mruan89@gmail.com>
2015-08-30 20:13:01 -0400

    sum_solver working for easy puzzle

--- sum_solver.py
+++ sum_solver.py
@@ -3,15 +3,17 @@ import numpy as np
 class Sum_Solver(object):
 
 	def __init__(self):
-		self.N = 9
-		self.N2 = self.N**2
+		self.n = 3
+		self.N  = self.n**2 # 9
+		self.N2 = self.N**2 # 81
+		self.N3 = self.N**3 # 729
 
 		self.raw_map = [list()] * self.N
 
 		self.todo = []
 
 		# C_mat.shape = (4*N^2, N^2+1) last col is count of 1s
-		self.C_mat = np.zeros((4*self.N2, self.N2+1), int)
+		self.C_mat = np.zeros((4*self.N2, self.N3+1), int)
 		self.C_mat[ : , -1] = self.N
 	
 	def print_raw(self):
@@ -46,15 +48,52 @@ class Sum_Solver(object):
 	def ind2rcl(self, ind):
 		return ((ind//self.N)%self.N, ind%self.N, ind//self.N2)
 
+	def init_SAT(self):
+
+		pair = [(i,j) for i in range(self.N) for j in range(self.N)]
+	
+		print(pair)
+		sat_count = 0
+		# For all levels
+		for (row, col) in pair:
+			for lev in range(self.N):
+				self.C_mat[sat_count, self.rcl2ind(row, col, lev)] = 1
+			sat_count += 1
+
+		# For all rows
+		for (col, lev) in pair:
+			for row in range(self.N):
+				self.C_mat[sat_count, self.rcl2ind(row, col, lev)] = 1
+			sat_count += 1
+
+		# For all columns
+		for (row, lev) in pair:
+			for col in range(self.N):
+				self.C_mat[sat_count, self.rcl2ind(row, col, lev)] = 1
+			sat_count += 1
+
+		# For all boxes
+		for (lev, box) in pair:
+			r = box // self.n * self.n
+			c = box %  self.n * self.n
+			for row in range(r, r+self.n):
+				for col in range(c, c+self.n):
+					self.C_mat[sat_count, self.rcl2ind(row, col, lev)] = 1
+			sat_count += 1  # one SAT per box
+
 	def phase_one(self):
 
 		while len(self.todo) > 0:
 			row, col, lev = self.todo.pop()
+
 			ind = self.rcl2ind(row, col, lev)
 
-			rows_todie, cols_todie = self.do_something(ind)
+			print(row, col, lev, ind)
 
-	def found_new_cell(self, ind):
+#			rows_todie, cols_todie = self.do_something(ind)
+			self.do_something(ind)
+
+	def found_new_cell(self, new_ind):
 
 		row, col, lev = self.ind2rcl(new_ind)
 		self.todo.append((row, col, lev))
@@ -66,10 +105,12 @@ class Sum_Solver(object):
 		# then remove all other variables
 		nz_r = np.nonzero(self.C_mat[:, v_ind])[0]
 
+#		print(self.C_mat[:, v_ind])
 		for row_i in nz_r:
 
 			# Find out the variables to be eliminated
-			nz_c = np.nonzero(self.C_mat[row_i, :-1])[0]
+			nz_cc = np.nonzero(self.C_mat[row_i, :-1]);
+			nz_c = nz_cc[0]
 
 			for col_j in nz_c:
 				# Their appearance in other rows are wiped out
@@ -85,13 +126,28 @@ class Sum_Solver(object):
 						new_ind = np.nonzero(self.C_mat[row_ii, :-1])
 						self.found_new_cell(new_ind[0][0])
 
-		return (nz_r, nz_c)
+#		print(nz_r)
+#		return (nz_r, nz_c)
 
 
 def test_rcl2ind():
 	pass
 
 
+def test_solver():
+
+	solver = Sum_Solver()
+
+	solver.read("game1.txt")
+	print("Input puzzle")
+	solver.print_raw()
+
+	solver.init_SAT()
+	solver.phase_one()
+
+	print("Output puzzle")
+	solver.print_raw()
 
 if __name__ == '__main__':
-	test_rcl2ind()
+#	test_rcl2ind()
+	test_solver()
